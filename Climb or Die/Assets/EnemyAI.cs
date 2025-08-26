8408b9abad27ae44249aa90b50651fa002353afe
Charkie25 <97203490+Charkie25@users.noreply.github.com>
2025-08-26 09:13:50 -0600

    AI Change
    Edits AI

--- Climb or Die/Assets/EnemyAI.cs
+++ Climb or Die/Assets/EnemyAI.cs
@@ -4,29 +4,25 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     private NavMeshAgent agent;
-
     public float AgentWanderRadius;
-
     public GameObject player;
+    private Rigidbody rb;
 
     void Start()
     {
         agent = gameObject.GetComponent<NavMeshAgent>();
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        // Vector2 point;
-        // if (Vector3.Distance(player.transform.position, transform.position) < AgentWanderRadius)
-        // {
-        //     point = Random.insideUnitSphere* AgentWanderRadius;
-        // }
-        // else
-        // {
-        //     point = new Vector2(player.transform.position.x, player.transform.position.z);
-        // }
-
         goToPosition(player.transform.position);
+
+        if (Vector3.Distance(player.transform.position, transform.position) > 50)
+        {
+            Vector3 directionToGo = (player.transform.position - transform.position).normalized;
+            rb.AddForce(directionToGo * 700);
+        }
     }
 
     private void goToPosition(Vector3 locaton)
