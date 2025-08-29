9d7ee0f1ca752ea84126494ac60d4c2a41cd78cc
Snail-of-the-snails <isaachendrixmiller@gmail.com>
2025-08-29 08:30:52 -0600

    Test

---
+++ Climb or Die/Assets/CanvasWake.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasWake : MonoBehaviour
+{
+    public Canvas canvas; // Reference to the Canvas component
+    public float alpha;
+    public CanvasGroup canvasGroup; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        canvasGroup = gameObject.GetComponent<CanvasGroup>();
+        alpha = canvasGroup.alpha;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+           alpha += 0.05f;
+    }
+}
