da1423a2dc3dc28946b53c39c4e654973ca24b3b
Jack Santucci <jackthesantucci@gmail.com>
2025-09-12 10:07:59 -0700

    working on restart button

--- Climb or Die/Assets/Scripts/KillPlayer.cs
+++ Climb or Die/Assets/Scripts/KillPlayer.cs
@@ -9,10 +9,13 @@ public class KillPlayer : MonoBehaviour
     public GameObject deathScreen;
     public GameObject crosshairAndStamina;
     public AudioListener audioListener;
+    Button button;
 
     void Start()
     {
         rb = transform.GetComponent<Rigidbody>();
+        button.enabled = false;
+
     }
 
     void Update()
@@ -35,5 +38,16 @@ public class KillPlayer : MonoBehaviour
         deathScreen.active = true;
         crosshairAndStamina.active = false;
         audioListener.enabled = false;
+        button.enabled = false;
+        for (float i = 0; i <= 1; i += 0.1f)
+        {
+            button.image.color = new Color(1, 1, 1, i);
+            StartCoroutine(DelayedAction());
+        }
+
+    }
+    IEnumerator DelayedAction()
+    {
+        yield return new WaitForSeconds(.2f); 
     }
 }
