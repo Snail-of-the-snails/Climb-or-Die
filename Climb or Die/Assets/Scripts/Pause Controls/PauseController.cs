823c7a7f45349f0811cc319bdb78ca8d808c2235
Jack Santucci <jackthesantucci@gmail.com>
2025-10-21 10:07:19 -0700

    Basically done with pause menu

--- Climb or Die/Assets/Scripts/Pause Controls/PauseController.cs
+++ Climb or Die/Assets/Scripts/Pause Controls/PauseController.cs
@@ -2,16 +2,27 @@ using UnityEngine;
 
 public class PauseController : MonoBehaviour
 {
+    public GameObject OptionsMenu;
+    public GameObject PauseMenu;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) )
         {
-            GameState currentGameState = GameStateManager.Instance.CurrentGameState;
-            GameState newGameState = currentGameState == GameState.Gameplay
-                ? GameState.Paused
-                : GameState.Gameplay;
+            if (OptionsMenu.activeSelf == false)
+            {
+                GameState currentGameState = GameStateManager.Instance.CurrentGameState;
+                GameState newGameState = currentGameState == GameState.Gameplay
+                    ? GameState.Paused
+                    : GameState.Gameplay;
 
-            GameStateManager.Instance.SetState(newGameState);
+                GameStateManager.Instance.SetState(newGameState);
+            }
+            else
+            {
+                OptionsMenu.SetActive(false);
+                PauseMenu.SetActive(true);
+            }
+           
         }
     }
 }
