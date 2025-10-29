f6932be0f8b4530920084b181ca93a594fc5b8a7
Jack Santucci <jackthesantucci@gmail.com>
2025-10-29 10:21:38 -0700

    added gun recoil and smoke particles
    also deleted the tree behaviour script.

--- Climb or Die/Assets/Scripts/Gun.cs
+++ Climb or Die/Assets/Scripts/Gun.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Audio;
 public class Gun : MonoBehaviour
 {
     public GameObject shotgun;
     private bool paused = false;
+    private bool canShoot = true;
 
     void Awake()
     {
@@ -18,9 +21,23 @@ public class Gun : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) && shotgun.activeSelf && !paused)
         {
-            transform.GetComponent<AudioSource>().Play();
+            if (canShoot)
+            {
+                canShoot = false;
+                StartCoroutine(fireGun());
+                
+            }
 
         }
+    }
+    IEnumerator fireGun()
+    {
+        transform.GetComponent<AudioSource>().Play();
+        shotgun.GetComponent<Animator>().Play("ShotgunFire");
+        yield return new WaitForSeconds(0.5f);
+        shotgun.GetComponent<Animator>().Play("New State");
+        canShoot = true;
+
     }
     private void OnGameStateChanged(GameState newGameState)
     {
